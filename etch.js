2a987b5f990e8462f27e8bda4537cb2b6407cc2b
yeaung00 <yep.aung00@gmail.com>
2022-06-02 13:00:06 -0700

    Added option for user inputted grid

--- etch.js
+++ etch.js
@@ -1,17 +1,32 @@
 const container = document.querySelector(".container");
+const gridDimensions = document.querySelector(".gridDimensions");
+let rowContainer;
+let column = 16;
+let row = 16;
 
-
-function createGrid() {
-    for(let i = 0; i <16; i++) {
-        const rowContainer = document.createElement("div");
+function createGrid(row,column) {
+    for(let i = 0; i <row; i++) {
+        rowContainer = document.createElement("div");
         rowContainer.setAttribute("class", "rowContainer");
         container.appendChild(rowContainer);
-        for(let i = 0; i <16; i++) {
+        for(let i = 0; i <column; i++) {
             const cells = document.createElement("div");
             cells.setAttribute("class", "cells");
             rowContainer.appendChild(cells);
         }
     }
 }
-createGrid();
+
+function deleteGrid() {
+    container.innerHTML = "";
+}
+
+gridDimensions.addEventListener('click', (e) => {
+    column = prompt("Input column size");
+    row = prompt("Input row size");
+    deleteGrid();
+    createGrid(row,column);
+})
+
+createGrid(row,column);
 
