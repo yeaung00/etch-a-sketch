5ef80eed6b26ed5285ca4d485a551ef70251d9ca
yeaung00 <yep.aung00@gmail.com>
2022-06-02 11:47:03 -0700

    Changed wording

--- etch.js
+++ etch.js
@@ -1,14 +1,15 @@
 const container = document.querySelector(".container");
 
+
 function createGrid() {
     for(let i = 0; i <16; i++) {
         const rowContainer = document.createElement("div");
         rowContainer.setAttribute("class", "rowContainer");
         container.appendChild(rowContainer);
         for(let i = 0; i <16; i++) {
-            const column = document.createElement("div");
-            column.setAttribute("class", "column");
-            rowContainer.appendChild(column);
+            const cells = document.createElement("div");
+            cells.setAttribute("class", "cells");
+            rowContainer.appendChild(cells);
         }
     }
 }
