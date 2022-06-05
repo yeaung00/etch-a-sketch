db728265cc7c8fe2709950523f23f551a6a72ec9
yeaung00 <yep.aung00@gmail.com>
2022-06-05 10:17:09 -0700

    added rainbow effect and fixed double click bug

--- etch.js
+++ etch.js
@@ -4,6 +4,7 @@ const eraserBtn = document.querySelector(".eraser");
 const clearBtn = document.querySelector(".clear");
 const colorPickerBtn = document.querySelector(".colorPicker");
 const markerBtn = document.querySelector(".marker");
+const rainbowBtn = document.querySelector(".rainbow");
 let showGridSize = document.querySelector(".showGridSize");
 let cells;
 let rowContainer;
@@ -12,7 +13,12 @@ let row = 16;
 let currentColor = "black";
 let prevColor = currentColor;
 let isDrawing = false;
-
+let red;
+let green;
+let blue;
+let isColorful = false;
+let isErasing = false;
+markerBtn.classList.add('selected');
 
 function createGrid(row,column) {
     for(let i = 0; i <row; i++) {
@@ -27,12 +33,21 @@ function createGrid(row,column) {
             rowContainer.appendChild(cells);
         }
     }
-    showGridSize.textContent = row + "x" + column;
+    showGridSize.textContent = row + " x " + column;
 }
-
 function isDown(e) {
     isDrawing = true;
+    if (isColorful) {
+        red = Math.floor(Math.random() * 256);
+        green = Math.floor(Math.random() * 256);
+        blue = Math.floor(Math.random() * 256);
+        currentColor = "rgb("+ red +","+ green +","+ blue +")";
+    }
+    if (isErasing) {
+        currentColor = 'white';
+    }
     e.target.style.backgroundColor = currentColor;
+    e.target.style.opacity = 1;
 }
 function deleteGrid() {
     container.innerHTML = "";
@@ -40,39 +55,58 @@ function deleteGrid() {
 markerBtn.addEventListener('click', marker);
 eraserBtn.addEventListener('click', erase);
 clearBtn.addEventListener('click', clear);
+rainbowBtn.addEventListener('click',rainbow);
 colorPickerBtn.oninput = (e) => currentColor = e.target.value;
 
+function rainbow() {
+    if (!isColorful) { prevColor = currentColor; }
+    
+    isErasing = false;
+    isColorful = true;
+    markerBtn.classList.remove('selected');
+    eraserBtn.classList.remove('selected');
+    rainbowBtn.classList.add('selected');
+}
+
 function marker() {
+    isColorful = false;
+    isErasing = false;
     markerBtn.classList.add('selected');
     eraserBtn.classList.remove('selected');
-    clearBtn.classList.remove('selected');
-    gridDimensions.classList.remove('selected');
+    rainbowBtn.classList.remove('selected');
     currentColor = prevColor;
 }
 function clear() {
-    markerBtn.classList.remove('selected');
-    eraserBtn.classList.remove('selected');
-    clearBtn.classList.add('selected');
-    gridDimensions.classList.remove('selected');
     deleteGrid();
     createGrid(row, column);
 }
 function erase(e) {
+    if (!isErasing) { prevColor = currentColor; }
+    
+    isColorful = false;
+    isErasing = true;
     markerBtn.classList.remove('selected');
     eraserBtn.classList.add('selected');
-    clearBtn.classList.remove('selected');
-    gridDimensions.classList.remove('selected');
-    prevColor = currentColor;
-    currentColor = "white";
+    rainbowBtn.classList.remove('selected');
+    
 }
 container.onmousedown = () => isDrawing = true;
 document.body.onmouseup = () => isDrawing = false;
 function paint(e) {
-        console.log(isDrawing);
-        if (!isDrawing) return;
-        e.target.style.backgroundColor = currentColor;
-        e.target.style.opacity = 1;
+
+    if (!isDrawing) return;
+    if (isColorful) {
+        red = Math.floor(Math.random() * 256);
+        green = Math.floor(Math.random() * 256);
+        blue = Math.floor(Math.random() * 256);
+        currentColor = "rgb("+ red +","+ green +","+ blue +")";
+    } else if (isErasing) {
+        currentColor = 'white';
+    }
     
+    e.target.style.backgroundColor = currentColor;
+    e.target.style.opacity = 1;
+
 }
 
 gridDimensions.addEventListener('click', (e) => {
